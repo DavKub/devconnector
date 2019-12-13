46c2a8b0553665b6ef77a365f20045ada7be30c3
David Kuboš <dav.kubos@gmail.com>
2019-12-13 16:50:25 +0100

    add user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', [
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    // See if user exists
+
+    // Get users gravatar
+
+    // Encrypt the password with bcrypt
+
+    // Return jsonwebtoken
+    
     res.send('User route');
 })
 
