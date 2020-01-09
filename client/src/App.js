c50ec1dfd090f482b4ddefb05148b3cf1d186783
David Kubos <dav.kubos@gmail.com>
2020-01-09 22:11:40 +0100

    Protected Route For Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import React, { Fragment, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRouting';
 // Redux
-import { Provider } from "react-redux";
-import store from "./store";
-import setAuthToken from "./utils/setAuthToken";
-import { loadUser } from "./actions/auth";
+import { Provider } from 'react-redux';
+import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
-import Alert from "./components/layout/Alert";
+import Alert from './components/layout/Alert';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
